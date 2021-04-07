b4eddac285549db75d2e2910d9ea56f1a210606c
Michael Styron <mts17b@my.fsu.edu>
2021-04-07 19:42:05 -0400

    -Updated PlayerList.java and list_of_players.xml
    -Added a black border around each textview
    -Added textviews for games and avg
    -PlayerList now fills in games and avg stats in the table

--- app/src/main/java/com/example/fantasybaseball/PlayerList.java
+++ app/src/main/java/com/example/fantasybaseball/PlayerList.java
@@ -23,6 +23,16 @@ public class PlayerList extends AppCompatActivity {
     TextView player3;
     TextView player4;
     TextView player5;
+    TextView games1;
+    TextView games2;
+    TextView games3;
+    TextView games4;
+    TextView games5;
+    TextView avg1;
+    TextView avg2;
+    TextView avg3;
+    TextView avg4;
+    TextView avg5;
     Button nextButton;
     Button prevButton;
     Cursor mCursor;
@@ -41,12 +51,22 @@ public class PlayerList extends AppCompatActivity {
         player3 = findViewById(R.id.playerName3);
         player4 = findViewById(R.id.playerName4);
         player5 = findViewById(R.id.playerName5);
+        games1 = findViewById(R.id.games1);
+        games2 = findViewById(R.id.games2);
+        games3 = findViewById(R.id.games3);
+        games4 = findViewById(R.id.games4);
+        games5 = findViewById(R.id.games5);
+        avg1 = findViewById(R.id.avg1);
+        avg2 = findViewById(R.id.avg2);
+        avg3 = findViewById(R.id.avg3);
+        avg4 = findViewById(R.id.avg4);
+        avg5 = findViewById(R.id.avg5);
 
         nextButton = findViewById(R.id.nextButton);
         prevButton = findViewById(R.id.previousButton);
 
         String[] mProjection = new String[] {playerContentProvider.COLUMN_NAME,
-                playerContentProvider.COLUMN_POSITION, playerContentProvider.COLUMN_HR};
+                playerContentProvider.COLUMN_GAMES, playerContentProvider.COLUMN_AVG};
         mCursor = getContentResolver().query(playerContentProvider.CONTENT_URI, mProjection,
                 null, null, null);
         createList(mCursor);
@@ -94,33 +114,57 @@ public class PlayerList extends AppCompatActivity {
     public void createList(Cursor mCursor){
         if(mCursor.moveToNext()){
             player1.setText(mCursor.getString(0));
+            games1.setText(String.valueOf(mCursor.getInt(1)));
+            avg1.setText(floatToString(mCursor.getFloat(2)));
         }
         else{
             player1.setText("");
+            games1.setText("");
+            avg1.setText("");
         }
         if(mCursor.moveToNext()){
             player2.setText(mCursor.getString(0));
+            games2.setText(String.valueOf(mCursor.getInt(1)));
+            avg2.setText(floatToString(mCursor.getFloat(2)));
         }
         else{
             player2.setText("");
+            games2.setText("");
+            avg2.setText("");
         }
         if(mCursor.moveToNext()){
             player3.setText(mCursor.getString(0));
+            games3.setText(String.valueOf(mCursor.getInt(1)));
+            avg3.setText(floatToString(mCursor.getFloat(2)));
         }
         else{
             player3.setText("");
+            games3.setText("");
+            avg3.setText("");
         }
         if(mCursor.moveToNext()){
             player4.setText(mCursor.getString(0));
+            games4.setText(String.valueOf(mCursor.getInt(1)));
+            avg4.setText(floatToString(mCursor.getFloat(2)));
         }
         else{
             player4.setText("");
+            games4.setText("");
+            avg4.setText("");
         }
         if(mCursor.moveToNext()){
             player5.setText(mCursor.getString(0));
+            games5.setText(String.valueOf(mCursor.getInt(1)));
+            avg5.setText(floatToString(mCursor.getFloat(2)));
         }
         else{
             player5.setText("");
+            games5.setText("");
+            avg5.setText("");
         }
+    }
+
+    public String floatToString(float num){
+        return String.format("%.3f", num);
     }
 }
